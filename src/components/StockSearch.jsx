3f614de30c2836bec0cd4df70385115f525633b4
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2025-03-20 15:22:58 -0400

    value range added

--- src/components/StockSearch.jsx
+++ src/components/StockSearch.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 const StockSearch = ({ onSearch }) => {
     const [query, setQuery] = useState("");
+    const [range, setRange] = useState("1mo"); //Default range 1month
 
     const handleSearch = () => {
-        if (query) onSearch(query.toUpperCase());
+        if (query) onSearch(query.toUpperCase(), range); //Passing range to onSearch
     };
 
     return (
@@ -16,6 +17,17 @@ const StockSearch = ({ onSearch }) => {
                 onChange={(e) => setQuery(e.target.value)}
                 className="border p-2 rounded w-full"
             />
+            {/*Data Range Selector*/}
+            <select
+                value={range}
+                onChange={(e) => setRange(e.target.value)}
+                className="border p-2 rounded"
+            >
+                <option value="1mo">1 Month</option>
+                <option value="6mo">6 Months</option>
+                <option value="1Y">1 Year</option>
+
+            </select>
             <button onClick={handleSearch} className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
                 Search
             </button>

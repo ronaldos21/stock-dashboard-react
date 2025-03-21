051dfb85cf7499587680ec821a045035444d9e3b
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2025-03-21 15:30:30 -0400

    small changes when darkmode implemented

--- src/components/StockCard.jsx
+++ src/components/StockCard.jsx
@@ -20,7 +20,7 @@ const StockCard = ({ stock }) => {
 
 
     return (
-        <div className="p-6 bg-white shadow-md rounded-lg w-full text-center">
+        <div className="p-6 bg-white dark:bg-gray-700 shadow-md rounded-lg w-full text-center">
             <h2 className="text-xl font-semibold">{stock.name} ({stock.symbol})</h2>
             <p className="text-3xl font-bold text-green-500">${livePrice}</p>
             <p className={`text-lg font-semibold ${stock.change > 0 ? "text-green-600" : "text-red-600"}`}>
@@ -28,7 +28,7 @@ const StockCard = ({ stock }) => {
             </p>
             <button
                 onClick={() => addToWatchlist(stock)}
-                className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                className="mt-4 px-4 py-2 bg-blue-600 dark:bg-blue-700 text-white rounded hover:bg-blue-700"
             >
                 ➕ Add to Watchlist
             </button>

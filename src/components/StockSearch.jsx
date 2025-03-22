77fb07d137d3d3a59c3b7703860d04eaf2b1294e
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2025-03-22 09:43:31 -0400

    style updated for searchbar and date range selector

--- src/components/StockSearch.jsx
+++ src/components/StockSearch.jsx
@@ -15,18 +15,17 @@ const StockSearch = ({ onSearch }) => {
                 placeholder="Enter stock symbol (e.g., AAPL)"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
-                className="border p-2 rounded w-full"
+                className="border p-2 rounded w-full bg-white dark:bg-gray-800 text-black dark:text-white border-gray-300 dark:border-gray-600"
             />
             {/*Data Range Selector*/}
             <select
                 value={range}
                 onChange={(e) => setRange(e.target.value)}
-                className="border p-2 rounded"
+                className="px-4 py-2 rounded bg-gray-200 dark:bg-gray-800 text-black dark:text-white"
             >
                 <option value="1mo">1 Month</option>
                 <option value="6mo">6 Months</option>
                 <option value="1Y">1 Year</option>
-
             </select>
             <button onClick={handleSearch} className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
                 Search
